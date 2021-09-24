13b6def001f9a09b2729c5d4c85d215302eacffb
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2021-09-24 13:59:25 +0200

    Update server.c

--- server.c
+++ server.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+// #include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main (int argc, char *argv[]) {
     int serverSock, binder, port, listener, accepting, clientLength;
