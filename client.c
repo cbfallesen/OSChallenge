9ba33edaf22bff964188b301a428b2cca3254b6d
cbfallesen <carolinefallesen@gmail.com>
2021-09-24 12:16:44 +0200

    created bare server

--- client.c
+++ client.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 int main () {
+
+    
+
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int main () {
+int main (int args) {
+    int serverSock, binder, port, listener, accepting;
+    struct sockaddr_in serverAddr, clientAddr;
+
+    port = args;
+
+    //Initialise the server address.
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = port;
+    
+    //Initialise server socket, using IPv4, stream socket and system default protocol.
+    serverSock = socket(AF_INET, SOCK_STREAM, 0);
+
+    //If socket returns -1, there is an error.
+    if (serverSock < 0) {
+        printf("Error connecting to socket");
+        return 1;
+    }
+
+    //bzero?
+
+    //Initialise the binder for the host address.
+    binder = bind(serverSock, (struct socketAddr *) &serverAddr, sizeof(serverAddr));
+
+    if (binder < 0) {
+        printf("Error binding the host address");
+        return 1;
+    }
+
+    //Initialise listener.
+    listener = listen(serverSock, 4);
+
+    accepting = accept(serverSock, (struct socketAddr *) &clientAddr, sizeof(clientAddr));
+
+    if (accepting < 0) {
+        printf("Error accepting client address");
+        return 1;
+    }
 
     return 0;
 }
