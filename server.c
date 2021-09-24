2910166e4817b92d78f97a7d20df2ea2ce15daa4
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2021-09-24 13:56:43 +0200

    Trying hardcoded localhost ip for server

--- server.c
+++ server.c
@@ -13,7 +13,8 @@ int main (int argc, char *argv[]) {
 
     //Initialise the server address.
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    // serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serverAddr.sin_port = port;
     
     //Initialise server socket, using IPv4, stream socket and system default protocol.
