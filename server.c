a75b7c60b9bb61fdf6bbac6b171aeb8c7273db5d
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2021-10-02 18:45:02 +0200

    Reverted to previous commit, debugging done.

--- server.c
+++ server.c
@@ -1,10 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+<<<<<<< HEAD
+=======
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main (int argc, char *argv[]) {
+    int serverSock, binder, port, listener, accepting, clientLength;
+    struct sockaddr_in serverAddr, clientAddr;
+
+    //Comment
+    // port = atoi(argv[1]);
+
+    char *ip = "127.0.0.1";
+    
+    //Initialise server socket, using IPv4, stream socket and system default protocol.
+    serverSock = socket(AF_INET, SOCK_STREAM, 0);
+
+    //If socket returns -1, there is an error.
+    if (serverSock < 0) {
+        printf("Error connecting to socket");
+        return 1;
+    }
+
+    //bzero?
+    bzero((char * ) &serverAddr, sizeof(serverAddr));
+    port = 5151;
+>>>>>>> parent of cbfcd71 (Binding host address to localhost)
 
 #include <netdb.h>
 #include <netinet/in.h>
 
+<<<<<<< HEAD
 #include <string.h>
+=======
+    //Initialise the server address.
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    // serverAddr.sin_addr.s_addr = inet_addr(ip);
+    serverAddr.sin_port = htons(port);
+    // serverAddr.sin_port = port;
+
+    //Initialise the binder for the host address.
+    binder = bind(serverSock, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+    if (binder < 0) {
+        printf("Error binding the host address");
+        return 1;
+    }
+
+    //Initialise listener.
+    listen(serverSock, 5);
+
+    clientLength = sizeof(clientAddr);
+    accepting = accept(serverSock, (struct sockaddr *) &clientAddr, &clientLength);
+>>>>>>> parent of cbfcd71 (Binding host address to localhost)
 
 int main( int argc, char *argv[] ) {
    int sockfd, newsockfd, portno, clilen;
