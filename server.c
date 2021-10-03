42d968642f5f844b23cde7bb588a86e993156772
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2021-10-03 19:06:43 +0200

    Updated headers

--- server.c
+++ server.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-// #include <netinet/in.h>
-#include <arpa/inet.h>
 
 int main (int argc, char *argv[]) {
     int serverSock, binder, port, listener, accepting, clientLength;
