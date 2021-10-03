972ac2466cffe0ab546eb42500c36054d693822d
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2021-10-03 19:42:31 +0200

    Update server.c

--- server.c
+++ server.c
@@ -58,7 +58,7 @@ int main( int argc, char *argv[] ) {
    
    printf("Message: %s \n",buffer);
    for (int i = 0; i < 49; ++i) {
-        printf("%c, ", buffer[i]);
+        printf("%c", buffer[i]);
     }
    
 
