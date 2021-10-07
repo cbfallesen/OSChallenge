f20c35c92ef1a0e8df83f46faa521fcf72725169
cbfallesen <carolinefallesen@gmail.com>
2021-10-07 15:15:01 +0200

    Added test

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char *argv[] ) {
+    char arr[] = {0,0,0,0,0,0,0,11};
+    
+    unsigned int i = atoi(arr);
+    printf("%d\n", i );
+    return 0;
+}
