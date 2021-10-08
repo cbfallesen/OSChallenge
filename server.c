c20f85f49a62652d79087b9adcc0d9b5e35b94eb
cbfallesen <carolinefallesen@gmail.com>
2021-10-08 16:32:59 +0200

    Update server.c

--- server.c
+++ server.c
@@ -79,7 +79,8 @@ int main( int argc, char *argv[] ) {
     printf("\n");
     
     
-    uint64_t *start = (uint64_t*) startArray;
+    uint64_t start;
+    memcpy(&start, startArray, 4);
 
     printf("%u", &start);
     printf("\n");
