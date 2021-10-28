e3d7f19974dbca23c1280694b57972735af301e0
cbfallesen <carolinefallesen@gmail.com>
2021-10-28 17:24:28 +0200

    deleted server

--- server.c
+++ server.c
@@ -1,122 +1,137 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <stdlib.h>
 #include <string.h>
-#include "messages.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-typedef struct Packet { 
-    char hashvalue[32]; 
-    uint64_t start; 
-    uint64_t end;
-    uint8_t p;
-} Packet;
+/* OpenSSL headers */
+#include <openssl/sha.h>
 
-uint64_t reverse(uint64_t start, uint64_t end, char *hash) {
-    unsigned char *targetHash = hash;
-    
-    for (uint8_t i = start; i < end; i++) {
-        //reverse endian
-        i = htole64(i);
-        
-        //Client forventer en binary string af hash
-        //Sørg for at parse request rigtigt, står som hints under rapporten
-        //Kan sende 1 request med 1 som det tal der skal hashes
-        //Når det virker kan vi gå videre til at fixe hashe
+#define MAX 49
+#define PORT 8080
+#define SA struct sockaddr
 
-        //Generates a SHA256 hash for the current iteration
-        SHA256_CTX shactx;
-        static unsigned char digest[SHA256_DIGEST_LENGTH];
-        SHA256_Init(&shactx);
-        SHA256_Update(&shactx, &i, SHA256_DIGEST_LENGTH);
-        SHA256_Final(digest, &shactx);
-        unsigned char *newHash = digest;
+typedef struct
+{
+	uint8_t hashvalue[32];
+	uint64_t start;
+	uint64_t end;
+	uint8_t p;
+} packet;
 
-        //Iterate through both hashes, to check if current iteration matches.
-        int j;
-        for (j = 0; j < SHA256_DIGEST_LENGTH; i++)
-        {
-            if(*(targetHash+j) != *(newHash+j)) {
-                break;
-            }
-        }
+// Function designed for chat between client and server.
+void func(int sockfd)
+{
+	char buff[MAX];
+	int n;
+	packet *Packet1;
 
-        if(j == SHA256_DIGEST_LENGTH - 1) {
-            return i;
-        }
+	bzero(buff, MAX);
 
-    }
+	// read the message from client and copy it in buffer
+	read(sockfd, buff, sizeof(buff));
+	Packet1 = (packet *)buff;
 
-    return -1;
+	// print buffer which contains the client contents
+	printf("\n\n");
+	int i;
+	for (i = 0; i < 32; i++)
+		printf("%02x", Packet1->hashvalue[i]);
+
+	printf("\nFrom start: %li end: %li priority: %d", be64toh(Packet1->start), be64toh(Packet1->end), Packet1->p);
+
+	uint64_t x;
+	uint64_t result;
+	result = -1;
+
+	for (x = be64toh(Packet1->start); x < be64toh(Packet1->end); x++)
+	{
+		unsigned char *guess = SHA256((unsigned char *)&x, 8, 0);
+
+		int equal = 1;
+		for (i = 0; i < 32; i++)
+		{
+			if (guess[i] != Packet1->hashvalue[i])
+			{
+				equal = 0;
+				break;
+			}
+		}
+
+		if (equal == 1)
+		{
+			result = x;
+			break;
+		}
+	}
+
+	result = htobe64(result);
+
+	// and send that buffer to client
+	write(sockfd, &result, sizeof(result));
+	// }
 }
 
-int main( int argc, char *argv[] ) {
-    int serverSock, newsockfd, portNum, clientLen;
-    struct sockaddr_in serv_addr, cli_addr;
-    
-    //Call the socket
-    serverSock = socket(AF_INET, SOCK_STREAM, 0);
-    
-    //Handling missing sockets
-    if (serverSock < 0) {
-        perror("ERROR opening socket");
-        exit(1);
-    }
-    
-    //Initialising the socket structures
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portNum = 5001;
-    
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portNum);
-    
-    //Binding to the host address + error handling
-    if (bind(serverSock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        perror("ERROR on binding");
-        exit(1);
-    }
+// Driver function
+int main()
+{
+	int sockfd, connfd, len;
+	struct sockaddr_in servaddr, cli;
 
-    listen(serverSock,5);
-    clientLen = sizeof(cli_addr);
-    
-    //Accept connection from the client
-    newsockfd = accept(serverSock, (struct sockaddr *)&cli_addr, &clientLen);
+	// socket create and verification
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+	{
+		printf("socket creation failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully created..\n");
+	bzero(&servaddr, sizeof(servaddr));
 
-    //Handling missing connection from the client
-    if (newsockfd < 0) {
-        perror("ERROR on accept");
-        exit(1);
-    }
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(PORT);
 
+	// Binding newly created socket to given IP and verification
+	if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0)
+	{
+		printf("socket bind failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully binded..\n");
 
-    char buffer[49];
-    //Start communicating, first read input from client
-    bzero(buffer,49);
-    read(newsockfd,buffer, 49);
-    
+	for (;;)
+	{
+		// Now server is ready to listen and verification
+		if ((listen(sockfd, 5)) != 0)
+		{
+			printf("Listen failed...\n");
+			exit(0);
+		}
+		else
+			printf("Server listening..\n");
+		len = sizeof(cli);
 
-    //Input from client
-    Packet packet = {buffer};
-    //Data skal parses rigtigt, vigtigt præcist format følges
-    unsigned char *hashArr = buffer;
-    uint64_t startR = be64toh(packet.start);
-    printf("%lu", startR);
-    uint64_t endR = be64toh(packet.end);
-    printf("%lu", endR);
+		// Accept the data packet from client and verification
+		connfd = accept(sockfd, (SA *)&cli, &len);
+		if (connfd < 0)
+		{
+			printf("server accept failed...\n");
+			exit(0);
+		}
+		else
+			printf("server accept the client...\n");
 
-    uint8_t outBuff[8];
-    bzero(outBuff, sizeof(outBuff)); 
-    printf("%c", outBuff);
+		// Function for chatting between client and server
+		func(connfd);
+	}
 
-    uint64_t ans = reverse(startR, endR, hashArr);
-    printf("%lu", ans);
-    uint64_t ansR = htobe64(ans);
-    printf("%lu", ansR);
-    memcpy(outBuff, &ansR, (size_t) 8);
-    
-    //Then write a response
-    write(newsockfd, outBuff, 8);
-
-    return 0;
+	// After chatting close the socket
+	close(sockfd);
 }
