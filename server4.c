fd41a705d62f893d5ed77074945ed5acd4c9e476
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2021-11-06 22:19:58 +0100

    Update server4.c

--- server4.c
+++ server4.c
@@ -128,6 +128,7 @@ void func(int sockfd)
 	pthread_mutex_destroy(&hashMutex);
 	// and send that buffer to client
 	write(sockfd, &result, sizeof(result));
+	free(&resultP);
 }
 
 // Driver function
