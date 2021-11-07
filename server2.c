01601ee159e99a5de6f54c0d958161eae95acf8f
cbfallesen <s194570@student.dtu.dk>
2021-11-07 15:42:45 +0100

    Update server2.c

--- server2.c
+++ server2.c
@@ -60,6 +60,7 @@ void func(int sockfd)
 	uint64_t end = Packet1->end;
 	uint64_t x;
 	uint64_t result;
+	printf("Inden resultArray");
 	uint8_t *resultArray[end - start][32];
 	printf("Inden for 1");
 	for(int i = 0; i < end-start; i++) {
