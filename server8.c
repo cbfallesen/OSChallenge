c1f714c587f1a75b24ec00085465284779ef0e36
cbfallesen <s194570@student.dtu.dk>
2021-11-19 18:08:26 +0100

    Create server8.c

---
+++ server8.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/* OpenSSL headers */
+#include <openssl/sha.h>
+
+#define MAX 49
+#define PORT 8080
+#define SA struct sockaddr
+
+typedef struct
+{
+	uint8_t hashvalue[32];
+	uint64_t start;
+	uint64_t end;
+	uint8_t p;
+} packet;
+
+struct Node
+{
+	packet data;
+	struct Node *next;
+};
+
+
+void* pushRequest (struct Node **refNode, packet *newData, size_t dataSize) {
+	struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+
+	//newNode->data = malloc(dataSize);
+	newNode->next = (*refNode);
+
+	newNode->data.start = newData->start;
+	newNode->data.end = newData->end;
+	newNode->data.p = newData->p;
+	memcpy(newNode->data.hashvalue, newData->hashvalue, sizeof(newData->hashvalue));
+
+	//(*refNode) = newNode;
+
+	return newNode;
+}
+
+struct Node *startNode = NULL;
+
+void print(struct Node *head) {
+    struct Node *current_node = head;
+   	while ( current_node != NULL) {
+        printf("%li \n", current_node->data.start);
+		printf("%li \n", current_node->data.end);
+		printf("%li \n", current_node->data.p);
+		for(int i = 0; i < 32; i++) {
+			printf("%li \n", current_node->data.hashvalue[i]);
+		}
+        current_node = current_node->next;
+    }
+}
+
+// Function designed for chat between client and server.
+// void func(int sockfd)
+// {
+// 	// print buffer which contains the client contents
+// 	printf("\n\n");
+// 	int i;
+// 	for (i = 0; i < 32; i++)
+// 		printf("%02x", Packet1->hashvalue[i]);
+
+// 	printf("\nFrom start: %li end: %li priority: %d", be64toh(Packet1->start), be64toh(Packet1->end), Packet1->p);
+
+// 	uint64_t x;
+// 	uint64_t result;
+// 	result = -1;
+
+// 	for (x = be64toh(Packet1->start); x < be64toh(Packet1->end); x++)
+// 	{
+// 		unsigned char *guess = SHA256((unsigned char *)&x, 8, 0);
+
+// 		int equal = 1;
+// 		for (i = 0; i < 32; i++)
+// 		{
+// 			if (guess[i] != Packet1->hashvalue[i])
+// 			{
+// 				equal = 0;
+// 				break;
+// 			}
+// 		}
+
+// 		if (equal == 1)
+// 		{
+// 			result = x;
+// 			break;
+// 		}
+// 	}
+
+// 	result = htobe64(result);
+
+// 	// and send that buffer to client
+// 	write(sockfd, &result, sizeof(result));
+// 	// }
+// }
+
+// Driver function
+int main()
+{
+	int sockfd, connfd, len;
+	struct sockaddr_in servaddr, cli;
+
+	// socket create and verification
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+	{
+		printf("socket creation failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully created..\n");
+	bzero(&servaddr, sizeof(servaddr));
+
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(PORT);
+
+	// Binding newly created socket to given IP and verification
+	if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0)
+	{
+		printf("socket bind failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully binded..\n");
+
+	for (;;)
+	{
+		// Now server is ready to listen and verification
+		if ((listen(sockfd, 5)) != 0)
+		{
+			printf("Listen failed...\n");
+			exit(0);
+		}
+		else
+			printf("Server listening..\n");
+		len = sizeof(cli);
+
+		// Accept the data packet from client and verification
+		connfd = accept(sockfd, (SA *)&cli, &len);
+		if (connfd < 0)
+		{
+			printf("server accept failed...\n");
+			exit(0);
+		}
+		else
+			printf("server accept the client...\n");
+		
+		char buff[MAX];
+		packet *Packet1;
+
+		bzero(buff, MAX);
+
+		// read the message from client and copy it in buffer
+		read(sockfd, buff, sizeof(buff));
+		Packet1 = (packet *)buff;
+
+		startNode = pushRequest(startNode, &Packet1, sizeof(Packet1));
+
+		print(startNode);
+	}
+	// Function for chatting between client and server
+	func(connfd);
+
+	// After chatting close the socket
+	close(sockfd);
+}
