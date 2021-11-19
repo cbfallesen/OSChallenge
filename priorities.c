5b223a9011de20c0463c5eff18a0e6e506d8e1eb
cbfallesen <s194570@student.dtu.dk>
2021-11-19 18:57:01 +0100

    Update priorities.c

--- priorities.c
+++ priorities.c
@@ -80,7 +80,7 @@ void func(int sockfd)
 	// 	printf("%02x", Packet1->hashvalue[i]);
 
 	// printf("\nFrom start: %li end: %li priority: %d\n", be64toh(Packet1->start), be64toh(Packet1->end), Packet1->p);
-	pushResult(&startNode, Packet1, sizeof(Packet1));
+	pushResult(&startNode, &Packet1, sizeof(Packet1));
 
 
 	// and send that buffer to client
